3bdde879113c83f70d4d4e0f9aae1b9615b55d0c
Kolby Kruger <kolbykruger@gmail.com>
2021-02-24 10:21:06 -0500

    design 2

--- js/scripts.js
+++ js/scripts.js
@@ -1,40 +1,73 @@
+//Sticky Header
+const header = document.querySelector('.header')
+const headerHeight = header.scrollHeight
+let lastScrollPos = 0
+
+window.addEventListener('scroll', function (e) {
+    stickyHeader()
+})
+
+document.addEventListener('DOMContentLoaded', function () {
+    stickyHeader()
+})
+
+function stickyHeader() {
+    let offsetTop = window.pageYOffset
+
+    if (offsetTop < headerHeight) {
+        header.classList.remove('header-sticky')
+    }
+
+    if (offsetTop >= lastScrollPos && offsetTop > headerHeight) {
+        header.classList.add('header-unsticky')
+        header.classList.remove('header-sticky')
+    }
+
+    if (offsetTop < lastScrollPos && offsetTop > headerHeight) {
+        header.classList.add('header-sticky')
+        header.classList.remove('header-unsticky')
+    }
+
+    lastScrollPos = offsetTop
+}
+
 //Blog Categories (if a selectbox)
 let blogCategories = {
     elem: document.querySelector('select#blog_categories'),
-    init: function() {
+    init: function () {
         if (this.elem) {
-            this.elem.addEventListener('change', function(e) {
+            this.elem.addEventListener('change', function (e) {
                 window.location.href = window.location.href.split('?')[0] + '?category=' + e.target.value
-            });
+            })
         }
-    }
-}.init();
+    },
+}.init()
 
 //Responsive iFrame
-$('iframe[src*="youtube"],iframe[src*="vimeo"]').wrap('<div class="responsive-iframe"/>');
+$('iframe[src*="youtube"],iframe[src*="vimeo"]').wrap('<div class="responsive-iframe"/>')
 
 //Accordion
-document.addEventListener('DOMContentLoaded', function() {
-    let accordion = document.querySelectorAll('.accordion-title');
+document.addEventListener('DOMContentLoaded', function () {
+    let accordion = document.querySelectorAll('.accordion-title')
 
     for (let i = 0; i < accordion.length; i++) {
-        accordion[i].addEventListener('click', function() {
-            let panel = this.nextElementSibling;
+        accordion[i].addEventListener('click', function () {
+            let panel = this.nextElementSibling
 
-            if (panel.style.maxHeight){
-                this.classList.remove('open');
-                panel.style.maxHeight = null;
-                panel.setAttribute('aria-hidden', true);
-                panel.setAttribute('aria-expanded', false);
+            if (panel.style.maxHeight) {
+                this.classList.remove('open')
+                panel.style.maxHeight = null
+                panel.setAttribute('aria-hidden', true)
+                panel.setAttribute('aria-expanded', false)
             } else {
-                this.classList.add('open');
-                panel.style.maxHeight = panel.scrollHeight + 'px';
-                panel.setAttribute('aria-hidden', false);
-                panel.setAttribute('aria-expanded', true);
+                this.classList.add('open')
+                panel.style.maxHeight = panel.scrollHeight + 'px'
+                panel.setAttribute('aria-hidden', false)
+                panel.setAttribute('aria-expanded', true)
             }
-        });
+        })
     }
-});
+})
 
 //Flickity Carousel
 $('.carousel .group').flickity({
@@ -46,7 +79,7 @@ $('.carousel .group').flickity({
     pageDots: true,
     imagesLoaded: true,
     autoPlay: 8000,
-});
+})
 
 $('.slideshow .container').flickity({
     cellSelector: '.slide',
@@ -57,9 +90,9 @@ $('.slideshow .container').flickity({
     pageDots: true,
     imagesLoaded: true,
     autoPlay: 5000,
-});
+})
 
-$('.impression .images').flickity({
+$('.impression .images, .work-with-us .images').flickity({
     cellSelector: '.images-item',
     wrapAround: true,
     adaptiveHeight: false,
@@ -68,29 +101,29 @@ $('.impression .images').flickity({
     pageDots: true,
     imagesLoaded: true,
     autoPlay: 5000,
-});
+})
 
 //Universal Tables
-$('table').wrap("<div class='universal-table'></div>");
+$('table').wrap("<div class='universal-table'></div>")
 
 //PDO Page loader
-document.addEventListener('DOMContentLoaded', function() {
-    let pdoElement = document.getElementById('pdopage');
+document.addEventListener('DOMContentLoaded', function () {
+    let pdoElement = document.getElementById('pdopage')
     if (pdoElement) {
-        let loadState = document.createElement('div');
-            loadState.classList.add('pdo-loader');
-            loadState.setAttribute('aria-hidden', true);
-            //loadState.textContent = 'Loading';
+        let loadState = document.createElement('div')
+        loadState.classList.add('pdo-loader')
+        loadState.setAttribute('aria-hidden', true)
+        //loadState.textContent = 'Loading';
 
-        pdoElement.appendChild(loadState);
+        pdoElement.appendChild(loadState)
 
         if (pdoPage) {
-            pdoPage.callbacks['before'] = function(config) {
-                document.querySelector('.pdo-loader').classList.add('pdo-loading');
-            };
-            pdoPage.callbacks['after'] = function(config) {
-                document.querySelector('.pdo-loader').classList.remove('pdo-loading');
-            };
+            pdoPage.callbacks['before'] = function (config) {
+                document.querySelector('.pdo-loader').classList.add('pdo-loading')
+            }
+            pdoPage.callbacks['after'] = function (config) {
+                document.querySelector('.pdo-loader').classList.remove('pdo-loading')
+            }
         }
     }
-});
+})
